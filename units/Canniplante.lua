606a62657cc19026b270185b48a0b1e9001fcfb1
Guthen <33220603+Guthen@users.noreply.github.com>
2019-02-26 10:50:06 +0100

    Added Canniplante to players units & Fix it

--- units/Canniplante.lua
+++ units/Canniplante.lua
@@ -5,7 +5,7 @@ return
 	hp = 200, -- ses points de vie
 	dmg = 100, -- ses points de dégats infligés
 	spd = 2.80, -- sa vitesse (1 = normal, inférieur à 1 = lent, supérieur à 1 = rapide)
-	attackRate = .25, -- en combien de secondes attaque t'il
+	attackRate = .45, -- en combien de secondes attaque t'il
 	cost = 15, -- combien l'unité coûte
 	isFly = true, -- s'il vole 
 	targetFly = true, -- s'il peut attaquer les unités volantes
