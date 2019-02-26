ba1c5120de7a8b94c7df1496b923c74019afca08
AdrienLerhit <46707850+AdrienLerhit@users.noreply.github.com>
2019-02-26 10:35:01 +0100

    Create Canniplante.lua

---
+++ units/Canniplante.lua
@@ -0,0 +1,14 @@
+return
+{
+	img = Image["Canniplante"], -- son nom d'image (sans l'extension)
+	name = "Canniplante", -- son nom dans le jeu
+	hp = 200, -- ses points de vie
+	dmg = 100, -- ses points de dégats infligés
+	spd = 2.80, -- sa vitesse (1 = normal, inférieur à 1 = lent, supérieur à 1 = rapide)
+	attackRate = .25, -- en combien de secondes attaque t'il
+	cost = 15, -- combien l'unité coûte
+	isFly = true, -- s'il vole 
+	targetFly = true, -- s'il peut attaquer les unités volantes
+	targetGround = true, -- s'il peut attaquer les unités terrestres
+	followTarget = true, -- s'il peut suivre sa cible
+}
