8b9cfc1e532524c0b7164b3cbaabbaccb0829b2e
Guthen <33220603+Guthen@users.noreply.github.com>
2019-04-25 18:59:53 +0200

    UI:CreateImage() + Game Logo

--- lua/ui.lua
+++ lua/ui.lua
@@ -9,8 +9,8 @@ function UI:CreateButton(x, y, w, h, center)
 		type = "Button",
 		id = #self.Objects,
 		img = nil,
-		x = center and (x or 0)-(w or 1)/2 or x or 0,
-		y = center and (y or 0)-(h or 1)/2 or y or 0,
+		x = x or 0,
+		y = y or 0,
 		w = w or 1,
 		h = h or 1,
 		doClick = print("UI: No function"),
@@ -23,6 +23,26 @@ function UI:CreateButton(x, y, w, h, center)
 	return but
 end
 
+function UI:CreateImage(x, y, w, h, img)
+	local _img = 
+	{
+		type = "Image",
+		id = #self.Objects,
+		img = img,
+		x = x or 0,
+		y = y or 0,
+		w = w or 1,
+		h = h or 1,
+	}
+	function _img:Remove()
+		table.remove( UI.Objects, id )
+	end
+	table.insert( self.Objects, _img )
+	return _img
+end
+
+-- > Object <  --
+
 function UI:ResetObject(id)
 	for k, v in pairs( self.Objects ) do
 		if v.type == id then table.remove( self.Objects, k ) end
@@ -34,7 +54,7 @@ end
 function UI:Update(dt)
 	for _, v in pairs( self.Objects ) do
 		if v.type == "Button" then
-			if v.img then v.w = v.img:getWidth() v.h = v.img:getHeight() end
+			--if v.img then v.w = v.img:getWidth() v.h = v.img:getHeight() end
 		end
 	end
 end
@@ -53,7 +73,8 @@ end
 function UI:Draw()
 	for _, v in pairs( self.Objects ) do
 		if v.img then
-			love.graphics.draw( v.img, v.x, v.y, 0 )
+			print( v.w )
+			love.graphics.draw( v.img, v.x, v.y, 0, v.w, v.h )
 		else
 			love.graphics.rectangle( "fill", v.x, v.y, v.w, v.h )
 		end
