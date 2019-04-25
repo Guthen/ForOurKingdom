ecf9ec0d28a601f6cd8d912cd6fcbba250fbf127
DolphiLerhit <adrienbo2zombie@gmail.com>
2019-04-25 18:55:29 +0100

    add Devoggs la troupe easter egg
    Il viole ton enfance et ta mere

---
+++ units/Devoggs.lua
@@ -0,0 +1,17 @@
+return
+{
+	img = Image["Trou noir"], -- son nom d'image (sans l'extension)
+	name = "Trou Noir", -- son nom dans le jeu
+	hp = 100000, -- ses points de vie
+	dmg = 50000, -- ses points de dégats infligés
+	spd = 1.5, -- sa vitesse (1 = normal, inférieur à 1 = lent, supérieur à 1 = rapide)
+	attackRate = 0, -- en combien de secondes attaque t'il
+	cost = 50, -- combien l'unité coûte
+	isFly = true, -- s'il vole 
+	targetFly = true, -- s'il peut attaquer les unités volantes
+	targetGround = true, -- s'il peut attaquer les unités terrestres
+	followTarget = true, -- s'il peut suivre sa cible
+	attackBase = true, -- s'il peut attaquer la base (optionnel)
+	canBeTarget = true, -- s'il peut être pris pour cible (optionnel)
+	animSpd = .06, -- temps en secondes avant de changer de passer à la prochaine image si vous avez une animation (optionnel)
+}
