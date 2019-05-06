03fc6555fd9dffc30b56cecc6279882ad60eed2f
DolphiLerhit <adrienbo2zombiegmail.com>
2019-05-06 13:54:54 +0200

    add anime plus troupe

---
+++ units/Goblance.lua
@@ -0,0 +1,15 @@
+return
+{
+	img = Image["Goblance"],
+	name = "Goblance",
+	hp = 50,
+	dmg = 15,
+	spd = 1.75,
+	attackRate = .5,
+	cost = 5,
+	isFly = false,
+	targetFly = true,
+	targetGround = true,
+	followTarget = false,
+	range = 3
+}
