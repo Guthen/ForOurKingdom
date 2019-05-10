fbe36097c67d3aadacc5d1c450c7cb41ff3781f8
DolphiLerhit <adrienbo2zombiegmail.com>
2019-05-10 10:35:52 +0200

    tour d'archer essai 1

---
+++ units/Tour_d'archer.lua
@@ -0,0 +1,16 @@
+return
+{
+	name = "Tour_d'archer",
+	img = Image["Tour-d'archer"],
+	rarety = 0, -- 0 : commun | 0 : rare | 0 : épique | 0 : mythique
+	hp = 0,
+	dmg = 75,
+	spd = 0.,
+	attackRate = .5,
+	cost = 0,
+	isFly = false,
+	targetFly = true,
+	targetGround = true,
+	followTarget = false,
+	range = 3
+}
