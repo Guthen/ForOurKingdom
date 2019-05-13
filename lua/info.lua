da509ac6c6097375dbcda3e799c4d5abd49ace2e
Thefallen59 <46729658+Thefallen59@users.noreply.github.com>
2019-05-13 21:18:00 +0200

    suite des info par units

---
+++ lua/info.lua
@@ -0,0 +1,9 @@
+Info = {}
+
+function Info:Units(name, hp, dmg, cost)
+	local inf = Info[name]
+	inf = Info[hp]
+	inf = Info[dmg]
+	inf = Info[cost]
+
+end
--- lua/menu.lua
+++ lua/menu.lua
@@ -218,13 +218,14 @@ function Menu:CreateInventory( ply )
 		  info.onDraw = function()
      	  		love.graphics.setColor(.1, .1, .1)
 				end
-				UI:CreateText(self.defX+155, self.defY-22.5+220, 1.98, 1.98, "Name: Assensorreur")
+				UI:CreateText(self.defX+155, self.defY-22.5+220, 1.98, 1.98, "Name: Ascensorreur")
 				UI:CreateText(self.defX+155, self.defY-22.5+250, 1.98, 1.98, "Heal: 10000")
 				UI:CreateText(self.defX+155, self.defY-22.5+280, 1.98, 1.98, "Domage: 10000")
 				UI:CreateText(self.defX+155, self.defY-22.5+310, 1.98, 1.98, "Speed: 1.4")
 				UI:CreateText(self.defX+155, self.defY-22.5+340, 1.98, 1.98, "Bio: ")
 				love.graphics.setColor(0, 0, 0)
 end
+
 
 function Menu:Key(k)
 end
