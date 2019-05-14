be128b32b790a1f6ec6de17c2fe7b08c3bff7c30
Thefallen59 <46729658+Thefallen59@users.noreply.github.com>
2019-05-14 10:36:45 +0200

    Modif Goblex

---
+++ units/Tinks.lua
@@ -0,0 +1,15 @@
+return 
+{
+	img = Image["Tinks"],
+	name = "Tinks",
+	rarety = 0, -- 0 : commun | 1 : rare | 2 : épique | 3 : mythique
+	hp = 100,
+	dmg = 45,
+	spd = 2,
+	attackRate = .75,
+	cost = 10,
+	isFly = true,
+	targetFly = true,
+	targetGround = true,
+	followTarget = true,
+}
