8e2290a04fb3dbd31f6b743a351201ddd46e818b
Guthen <33220603+Guthen@users.noreply.github.com>
2019-06-07 09:17:10 +0200

    attempted to fix

--- units/Bouble de feu.lua
+++ units/Bouble de feu.lua
@@ -1,4 +1,4 @@
-return
+﻿return
 {
 	desc = 
 		"Le Bouble de Feu est un sort créé\n par les plus incompétants Sorciers de Feu.\nIl servirait à brûler les autres créatures.",
@@ -6,9 +6,10 @@ return
 	deadImg = Image["Boule de feu attack"], -- son image à la mort de celui-ci (fonctionne seulement si dieToFirstKill = true)
 	rarety = 1, -- 0 : commun | 1 : rare | 2 : épique | 3 : mythique
 	name = "Bouble de Feu", -- son nom dans le jeu
+	lvl = 1,
 	fx = "fx_fire_explosion",
 	hp = 10, -- ses points de vie
-	dmg = 10000, -- ses points de dégats infligés
+	dmg = 1000, -- ses points de dégats infligés
 	spd = 8, -- sa vitesse (1 = normal, inférieur à 1 = lent, supérieur à 1 = rapide)
 	attackRate = 0, -- en combien de secondes attaque t'il
 	cost = 15, -- combien l'unité coûte
@@ -21,12 +22,9 @@ return
 	dieToFirstKill = true, -- s'il meurt à son premier "kill"
 	animSpd = 0.35, -- temps en secondes avant de changer de passer à la prochaine image si vous avez une animation (optionnel)
 	onSpawn = function() -- fonction exécuté à l'appariton de l'unité
-		local snd = Sound["fireball"]
-		snd:setVolume(.25)
-		snd:play()
+		Sound:Play("fireball", 0.5, true)
 	end,
 	onDestroyed = function() -- fonction exécuté à la mort de l'unité
-		local snd = Sound["fireball"]
-		snd:stop()
+		Sound:Stop("fireball")
 	end,
 }
